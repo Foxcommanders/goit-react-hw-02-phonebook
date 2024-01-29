19cf581a826a7fd9889c60ba8444082f370f5f28
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2024-01-29 11:00:15 +0200

    Update all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import Form from './Form/Form';
+import ContactList from './ContactList/ContactList';
+import Section from './Section/Section';
+import Filter from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  onDeleteContact = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+  };
+
+  onAddContact = contactData => {
+    const checkedContact = this.state.contacts.find(
+      contact => contactData.name === contact.name
+    );
+    if (checkedContact) {
+      alert(`${contactData.name} is already in contacts`);
+      return;
+    } else {
+      const contact = { id: nanoid(), ...contactData };
+      this.setState({ contacts: [contact, ...this.state.contacts] });
+    }
+  };
+
+  onFilter = filterData => {
+    this.setState({ filter: filterData });
+  };
+
+  render() {
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name
+        .toLowerCase()
+        .includes(this.state.filter.toLowerCase().trim())
+    );
+
+    return (
+      <Section>
+        <h1>
+          <span>☎︎ </span>Phonebook
+        </h1>
+        <Form onAddContact={this.onAddContact} />
+        <h2>Contacts</h2>
+        <Filter onFilter={this.onFilter} filter={this.state.filter} />
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={this.onDeleteContact}
+        />
+      </Section>
+    );
+  }
+}
